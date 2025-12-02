645f0925b71e898b940e8e6e855edcabda057b9e
Alexander Herder <alexander.herder@gmail.com>
2025-12-02 14:07:52 +0100

    Initial commit

---
+++ Trad.cpp
@@ -0,0 +1,52 @@
+// Alexander Herder
+
+//  Trad.cpp
+//  labb4_tree
+
+#include <iostream>
+#include "Trad.h"
+
+
+void Trad::kopiera( const Trad& t)
+{
+  //std::cout << "*** Trad::kopiera\n";
+  if (t.tomt())
+    rot = 0;
+  else
+  {
+    rot = new Nod( t.varde());
+    v_barn().kopiera( t.v_barn());
+    h_barn().kopiera( t.h_barn());
+  }
+}
+
+Trad& Trad::operator= (const Trad& t)
+{
+  //std::cout << "*** Trad::operator=\n";
+  if (rot != t.rot)
+  {
+    delete rot;
+    kopiera(t);
+  }
+  return *this;
+}
+
+bool Trad::operator== (const Trad& t) const
+{
+  //std::cout << "*** Trad::operator==\n";
+  return (tomt() && t.tomt()) ||
+          (!tomt() && !t.tomt() && varde() == t.varde() &&
+           v_barn() == t.v_barn() && h_barn() == t.h_barn());
+}
+
+void Trad::skriv_ut() const
+{
+  // traversera igenom trädet rekursivt enligt principen "in-order"
+  if (!tomt())
+  {
+    v_barn().skriv_ut();
+    std::cout << varde() << ' ';
+    h_barn().skriv_ut();
+  }
+}
+
---
+++ Trad.h
@@ -0,0 +1,62 @@
+// Alexander Herder
+
+//  Trad.h
+//  labb4_tree
+
+
+#ifndef TRAD_H
+#define TRAD_H
+
+#include <iostream>
+
+class Trad {
+  
+public:
+  Trad() : rot(0) {};                    // default konstruktor
+  Trad( int d ) { rot= new Nod(d); };
+  Trad( const Trad& t ) { kopiera(t); };   // kopieringskonstruktor
+  ~Trad() { delete rot; };
+  
+  bool tomt() const { return !rot; };
+  int& varde() const
+  {
+    koll();
+    return rot->data;
+  };
+  Trad& v_barn() const
+  {
+    koll();
+    return *rot->vanster;
+  };
+  Trad& h_barn() const
+  {
+    koll();
+    return *rot->hoger;
+  };
+  Trad& operator= ( const Trad& );
+  bool operator== ( const Trad& ) const;
+  void skriv_ut() const;
+  
+private:
+  class Nod {
+    friend class Trad;
+    int data;
+    Trad *vanster, *hoger;
+    Nod( int d ) : data( d ), vanster( new Trad ), hoger( new Trad ) {};
+    ~Nod()
+    {
+      delete vanster;
+      delete hoger;
+    };
+  }; // end class Nod
+
+  Nod *rot;
+  void koll() const
+  {
+    if ( tomt() )
+      throw std::range_error( "Trad" );
+  }
+  void kopiera( const Trad& t );
+};
+
+#endif /* Trad_h */
