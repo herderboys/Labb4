bf8a4c86295992bdaf5782dd777cf23f884dd86a
Alexander Herder <alexander.herder@gmail.com>
2025-12-02 14:08:40 +0100

    Fix initial indentation

--- Trad.cpp
+++ Trad.cpp
@@ -7,46 +7,45 @@
 #include "Trad.h"
 
 
-void Trad::kopiera( const Trad& t)
+void Trad::kopiera(const Trad& t)
 {
-  //std::cout << "*** Trad::kopiera\n";
-  if (t.tomt())
-    rot = 0;
-  else
-  {
-    rot = new Nod( t.varde());
-    v_barn().kopiera( t.v_barn());
-    h_barn().kopiera( t.h_barn());
-  }
+    //std::cout << "*** Trad::kopiera\n";
+    if (t.tomt())
+        rot = 0;
+    else
+    {
+        rot = new Nod(t.varde());
+        v_barn().kopiera(t.v_barn());
+        h_barn().kopiera(t.h_barn());
+    }
 }
 
-Trad& Trad::operator= (const Trad& t)
+Trad& Trad::operator=(const Trad& t)
 {
-  //std::cout << "*** Trad::operator=\n";
-  if (rot != t.rot)
-  {
-    delete rot;
-    kopiera(t);
-  }
-  return *this;
+    //std::cout << "*** Trad::operator=\n";
+    if (rot != t.rot)
+    {
+        delete rot;
+        kopiera(t);
+    }
+    return *this;
 }
 
-bool Trad::operator== (const Trad& t) const
+bool Trad::operator==(const Trad& t) const
 {
-  //std::cout << "*** Trad::operator==\n";
-  return (tomt() && t.tomt()) ||
-          (!tomt() && !t.tomt() && varde() == t.varde() &&
-           v_barn() == t.v_barn() && h_barn() == t.h_barn());
+    //std::cout << "*** Trad::operator==\n";
+    return (tomt() && t.tomt()) ||
+    (!tomt() && !t.tomt() && varde() == t.varde() &&
+        v_barn() == t.v_barn() && h_barn() == t.h_barn());
 }
 
 void Trad::skriv_ut() const
 {
-  // traversera igenom trädet rekursivt enligt principen "in-order"
-  if (!tomt())
-  {
-    v_barn().skriv_ut();
-    std::cout << varde() << ' ';
-    h_barn().skriv_ut();
-  }
+    // traversera igenom trädet rekursivt enligt principen "in-order"
+    if (!tomt())
+    {
+        v_barn().skriv_ut();
+        std::cout << varde() << ' ';
+        h_barn().skriv_ut();
+    }
 }
-
--- Trad.h
+++ Trad.h
@@ -9,54 +9,66 @@
 
 #include <iostream>
 
-class Trad {
-  
+class Trad
+{
 public:
-  Trad() : rot(0) {};                    // default konstruktor
-  Trad( int d ) { rot= new Nod(d); };
-  Trad( const Trad& t ) { kopiera(t); };   // kopieringskonstruktor
-  ~Trad() { delete rot; };
-  
-  bool tomt() const { return !rot; };
-  int& varde() const
-  {
-    koll();
-    return rot->data;
-  };
-  Trad& v_barn() const
-  {
-    koll();
-    return *rot->vanster;
-  };
-  Trad& h_barn() const
-  {
-    koll();
-    return *rot->hoger;
-  };
-  Trad& operator= ( const Trad& );
-  bool operator== ( const Trad& ) const;
-  void skriv_ut() const;
-  
-private:
-  class Nod {
-    friend class Trad;
-    int data;
-    Trad *vanster, *hoger;
-    Nod( int d ) : data( d ), vanster( new Trad ), hoger( new Trad ) {};
-    ~Nod()
+    Trad() : rot(0)
     {
-      delete vanster;
-      delete hoger;
-    };
-  }; // end class Nod
+    }; // default konstruktor
+    Trad(int d) { rot = new Nod(d); };
+    Trad(const Trad& t) { kopiera(t); }; // kopieringskonstruktor
+    ~Trad() { delete rot; };
 
-  Nod *rot;
-  void koll() const
-  {
-    if ( tomt() )
-      throw std::range_error( "Trad" );
-  }
-  void kopiera( const Trad& t );
+    bool tomt() const { return !rot; };
+
+    int& varde() const
+    {
+        koll();
+        return rot->data;
+    };
+
+    Trad& v_barn() const
+    {
+        koll();
+        return *rot->vanster;
+    };
+
+    Trad& h_barn() const
+    {
+        koll();
+        return *rot->hoger;
+    };
+    Trad& operator=(const Trad&);
+    bool operator==(const Trad&) const;
+    void skriv_ut() const;
+
+private:
+    class Nod
+    {
+        friend class Trad;
+        int data;
+        Trad *vanster, *hoger;
+
+        Nod(int d) : data(d), vanster(new Trad), hoger(new Trad)
+        {
+        };
+
+        ~Nod()
+        {
+            delete vanster;
+            delete hoger;
+        };
+    }; // end class Nod
+
+    Nod* rot;
+
+    void koll() const
+    {
+        if (tomt())
+            throw std::range_error("Trad");
+    }
+
+    void kopiera(const Trad& t);
 };
 
 #endif /* Trad_h */
