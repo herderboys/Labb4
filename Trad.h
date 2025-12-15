d062f4bfd4b633efc26c9591209b0cf7f1c219bb
Alexander Herder <alexander.herder@gmail.com>
2025-12-15 09:57:26 +0100

    Add full functionality, fix bugs

--- Trad.h
+++ Trad.h
@@ -9,66 +9,198 @@
 
 #include <iostream>
 
+template <typename T>
 class Trad
 {
 public:
-    Trad() : rot(0)
+    // default constructor
+    Trad() : root(nullptr)
     {
-    }; // default konstruktor
-    Trad(int d) { rot = new Nod(d); };
-    Trad(const Trad& t) { kopiera(t); }; // kopieringskonstruktor
-    ~Trad() { delete rot; };
+    }
 
-    bool tomt() const { return !rot; };
-
-    int& varde() const
+    // constructor with initialized data
+    Trad(T data)
     {
-        koll();
-        return rot->data;
-    };
+        root = new Node(data);
+    }
 
-    Trad& v_barn() const
+    // copy constructor
+    Trad(const Trad& tree)
     {
-        koll();
-        return *rot->vanster;
-    };
+        copy(tree);
+    }
 
-    Trad& h_barn() const
+    // destructor
+    ~Trad()
     {
-        koll();
-        return *rot->hoger;
-    };
-    Trad& operator=(const Trad&);
-    bool operator==(const Trad&) const;
-    void skriv_ut() const;
+        delete root;
+    }
+
+
+    // empty check
+    bool empty() const
+    {
+        return !root;
+    }
+
+    // check what data root has
+    T& value() const
+    {
+        check();
+        return root->data;
+    }
+
+
+    // check which left children root has
+    Trad& l_children() const
+    {
+        check();
+        return *root->left;
+    }
+
+    // check which right children root has
+    Trad& r_children() const
+    {
+        check();
+        return *root->right;
+    }
+
+
+    // public insertion method
+    void satt_in(T data)
+    {
+        insert(data);
+    }
+
+    // assignment operator
+    Trad& operator=(const Trad& other)
+    {
+        // std::cout << "*** Trad::operator=\n";
+        if (root != other.root)
+        {
+            delete root;
+            copy(other);
+        }
+        return *this;
+    }
+
+    // equals operator
+    bool operator==(const Trad& t) const
+    {
+        // std::cout << "*** Trad::operator==\n";
+        return (empty() && t.empty()) ||
+        (!empty() && !t.empty() && value() == t.value() &&
+            l_children() == t.l_children() && r_children() == t.r_children());
+    }
+
+
+    // print out method
+    void skriv_ut() const
+    {
+        // traversera igenom trädet rekursivt enligt principen "in-order"
+        if (!empty())
+        {
+            l_children().skriv_ut();
+            std::cout << value() << ' ';
+            r_children().skriv_ut();
+        }
+    }
+
+
+    // public search function
+    T sok(T value)
+    {
+        return search(value);
+    }
 
 private:
-    class Nod
+    class Node
     {
         friend class Trad;
-        int data;
-        Trad *vanster, *hoger;
+        T data;
+        Trad *left, *right;
 
-        Nod(int d) : data(d), vanster(new Trad), hoger(new Trad)
-        {
-        };
 
-        ~Nod()
+        // node constructor
+        Node(T data) : data(data), left(new Trad), right(new Trad)
         {
-            delete vanster;
-            delete hoger;
-        };
+        }
+
+        // node destructor
+        ~Node()
+        {
+            delete left;
+            delete right;
+        }
     }; // end class Nod
 
-    Nod* rot;
 
-    void koll() const
+    Node* root;
+
+
+    // private insertion function
+    void insert(T data)
     {
-        if (tomt())
+        if (empty())
+        {
+            root = new Node(data);
+            return;
+        }
+
+        // if (data == value())
+
+        //     do nothing
+
+        if (data < value())
+        {
+            l_children().insert(data);
+        }
+        if (data > value())
+        {
+            r_children().insert(data);
+        }
+    }
+
+
+    // private search function
+    T search(T target)
+    {
+        if (empty())
+        {
+            return 0;
+        }
+        if (value() == target)
+        {
+            return target;
+        }
+        if (value() > target)
+        {
+            return l_children().search(target);
+        }
+        return r_children().search(target);
+    }
+
+
+    // check if tree is empty
+    void check() const
+    {
+        if (empty())
             throw std::range_error("Trad");
     }
 
-    void kopiera(const Trad& t);
+    // copy method
+    void copy(const Trad& t)
+    {
+        // std::cout << "*** Trad::kopiera\n";
+        if (t.empty())
+            root = 0;
+        else
+        {
+            root = new Node(t.value());
+            l_children().copy(t.l_children());
+            r_children().copy(t.r_children());
+        }
+    }
 };
 
 #endif /* Trad_h */
--- labb4_main.cpp
+++ labb4_main.cpp
@@ -7,7 +7,6 @@
 #include "Trad.h"
 
 
-/* 
 // Denna block-kommentar måste tas bort när programmet skall VPL-evalueras!
  
 // Funktionen nedan används för test/evaluering av din lösning.
@@ -15,10 +14,10 @@
 // "print_eval_double", du behöver därmed revidera funktionen,
 // så att den kan anropas av båda funktionerna.
 // Tänk på att inte ändra utskrift instruktionerna då dessa används vid VPL-evalueringen.
-
-void inmatning( Trad& t )
+template<typename T>
+void inmatning( Trad<T>& t )
 {
-  int i;
+  T i;
   std::cout << "Mata in tal: "; std::cin >> i; //2
   if ( t.sok( i ) )
     std::cout << "Finns redan i trädet!" << std::endl;
@@ -91,39 +90,38 @@ void print_eval_double( Trad<double> & tf, Trad<double> & tf2 )
   tf2.skriv_ut();
   std::cout << std::endl;
 }
-*/ //-SLUT på block-kommentar som skall tas bort när programmet skall evalueras!
+//-SLUT på block-kommentar som skall tas bort när programmet skall evalueras!
 
 
 // Denna funktion visar principen av ett rekursivt binärt sökträd, anropas bara i de initala filerna. 
 // Skall alltså inte anropas när det är dags för VPL-evaluering.
-void inmatning_och_print_first( Trad &t, Trad &t2 )
-{
-  t = Trad(5);
-  t.v_barn() = Trad(4);
-  t.h_barn() = Trad(7);
-  t.v_barn().v_barn() = Trad(1);
-  t.v_barn().v_barn().h_barn() = Trad(3);
-  
-  t.h_barn().v_barn() = Trad(6);
-  t.h_barn().h_barn() = Trad(8);
-  t.h_barn().h_barn().h_barn() = Trad(9);
-  
-  t2 = t;
-  t2.skriv_ut(); std::cout << std::endl;
-  if ( t == t2 )
-    std::cout << "LIKA" << std::endl;
-  else
-    std::cout << "OLIKA" << std::endl;
-  
-  t2.h_barn().h_barn().v_barn() = Trad(7);
-  if ( t == t2 )
-    std::cout << "LIKA" << std::endl;
-  else
-    std::cout << "OLIKA" << std::endl;
-}
+// void inmatning_och_print_first( BinarySearchTree &t, BinarySearchTree &t2 )
+// {
+//   t = BinarySearchTree(5);
+//   t.l_children() = BinarySearchTree(4);
+//   t.r_children() = BinarySearchTree(7);
+//   t.l_children().l_children() = BinarySearchTree(1);
+//   t.l_children().l_children().r_children() = BinarySearchTree(3);
+//
+//   t.r_children().l_children() = BinarySearchTree(6);
+//   t.r_children().r_children() = BinarySearchTree(8);
+//   t.r_children().r_children().r_children() = BinarySearchTree(9);
+//
+//   t2 = t;
+//   t2.print_out(); std::cout << std::endl;
+//   if ( t == t2 )
+//     std::cout << "LIKA" << std::endl;
+//   else
+//     std::cout << "OLIKA" << std::endl;
+//
+//   t2.r_children().r_children().l_children() = BinarySearchTree(7);
+//   if ( t == t2 )
+//     std::cout << "LIKA" << std::endl;
+//   else
+//     std::cout << "OLIKA" << std::endl;
+// }
 
 int main() {
-  /*
   //Denna block-kommentar måste tas bort när programmet skall VPL-evalueras!
   // *********************************
   // Använd följande för VPL-evaluering
@@ -138,12 +136,12 @@ int main() {
   
   // Slut på det som används för VPL-evaluering
   // *********************************
-  */ //-SLUT på block-kommentar som skall tas bort när programmet skall evalueras!
+ //-SLUT på block-kommentar som skall tas bort när programmet skall evalueras!
   
   
   // Följande initiala kod, måste bortkommenteras när programmet skall VPL-evalueras!
-  Trad t, t2;
-  inmatning_och_print_first( t, t2 );
+ // BinarySearchTree t, t2;
+  // inmatning_och_print_first( t, t2 );
   
   
   return 0;
